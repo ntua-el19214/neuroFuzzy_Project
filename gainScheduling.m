e9bcc03da86230e6111ede983d6565d343305058
ntua-el19214 <108694882+ntua-el19214@users.noreply.github.com>
2024-11-13 23:58:51 +0200

    Updates, made initial main script, changes acceleration acalculation in dynamics

--- gainScheduling.m
+++ gainScheduling.m
@@ -1,4 +1,4 @@
-function gainMat = gainScheduling(curretSA, ssVectorSA, ExpandedMatrices)
+function gainMat = gainScheduling(curretSA, ssVectorSteerA, ExpandedMatrices)
 %Define an algorithmic method to switch between gains, when operating
 %points change
 
@@ -7,19 +7,19 @@ gainMat = struct('Kr', zeros([size(ExpandedMatrices(1).Kr)]), 'Ki', zeros([size(
 for iOP = 1:5
     if iOP == 1
         % Special case for the first element
-        degreeOfMembership = triangularPulse(2*ssVectorSA(1), ssVectorSA(1), ssVectorSA(2), curretSA);
+        degreeOfMembership = triangularPulse(2*ssVectorSteerA(1), ssVectorSteerA(1), ssVectorSteerA(2), curretSA);
         gainMat.Kr = gainMat.Kr + degreeOfMembership.*ExpandedMatrices(iOP).Kr;
         gainMat.Ki = gainMat.Ki + degreeOfMembership.*ExpandedMatrices(iOP).Ki;
         gainMat.Kp = gainMat.Kp + degreeOfMembership.*ExpandedMatrices(iOP).Kp;
     elseif iOP == 5
         % Special case for the last element
-        degreeOfMembership = triangularPulse(2*ssVectorSA(4), ssVectorSA(5), 2*ssVectorSA(5), curretSA);
+        degreeOfMembership = triangularPulse(2*ssVectorSteerA(4), ssVectorSteerA(5), 2*ssVectorSteerA(5), curretSA);
         gainMat.Kr = gainMat.Kr + degreeOfMembership.*ExpandedMatrices(iOP).Kr;
         gainMat.Ki = gainMat.Ki + degreeOfMembership.*ExpandedMatrices(iOP).Ki;
         gainMat.Kp = gainMat.Kp + degreeOfMembership.*ExpandedMatrices(iOP).Kp;
     else
         % General case for elements 2 to 4
-        degreeOfMembership = triangularPulse(ssVectorSA(iOP-1), ssVectorSA(iOP), ssVectorSA(iOP+1), curretSA);
+        degreeOfMembership = triangularPulse(ssVectorSteerA(iOP-1), ssVectorSteerA(iOP), ssVectorSteerA(iOP+1), curretSA);
         gainMat.Kr = gainMat.Kr + degreeOfMembership.*ExpandedMatrices(iOP).Kr;
         gainMat.Ki = gainMat.Ki + degreeOfMembership.*ExpandedMatrices(iOP).Ki;
         gainMat.Kp = gainMat.Kp + degreeOfMembership.*ExpandedMatrices(iOP).Kp;
--- motorTorque.m
+++ motorTorque.m
@@ -4,7 +4,7 @@ maxGripLimitedFx = zeros(1,length(wheel_omega_array));
 
 torqueRatio = (input(1)+input(3))/(input(2) + input(4));
 for iMotor = 1:length(wheel_omega_array)
-    maxFx = max(vehicle.Motors(wheel_omega_array(iMotor)*vehicle.GR))*vehicle.GR/vehicle.Reff;
+    maxFx = max(vehicle.Motors(wheel_omega_array(iMotor)*vehicle.GR))*vehicle.GR/vehicle.R;
     maxGripLimitedFx(iMotor) = min(maxFx, vehicle.TireMaxFx(slipAngleMatrix(iMotor), wheel_fz_array(iMotor)));
 end
 gripRatio = (maxGripLimitedFx(1)+maxGripLimitedFx(3))/(maxGripLimitedFx(2) + maxGripLimitedFx(4));
@@ -15,24 +15,24 @@ if gripRatio < torqueRatio % the grip limit is on the left side
     % example: If the requested split is 2:1 and available grip is
     % 1.75:1.15, then we apply all 1.75 torque that is available to the
     % left track and apply 1.75/2 to the right track
-    Tmotor(1) = maxGripLimitedFx(1)*input(1)/(input(1)+input(3))/vehicle.GR*vehicle*Reff;
-    Tmotor(3) = maxGripLimitedFx(3)*input(3)/(input(1)+input(3))/vehicle.GR*vehicle*Reff;
+    Tmotor(1) = maxGripLimitedFx(1)*input(1)/(input(1)+input(3))/vehicle.GR*vehicle.R;
+    Tmotor(3) = maxGripLimitedFx(3)*input(3)/(input(1)+input(3))/vehicle.GR*vehicle.R;
     
     totalFxRight = (maxGripLimitedFx(1)*input(1)/(input(1)+input(3)) + maxGripLimitedFx(3)*input(3)/(input(1)+input(3)))/torqueRatio;
     
     
-    Tmotor(2) = totalFxRight*input(2)/(input(2)+input(4))/vehicle.GR*vehicle*Reff;
-    Tmotor(4) = totalFxRight*input(4)/(input(2)+input(4))/vehicle.GR*vehicle*Reff;
+    Tmotor(2) = totalFxRight*input(2)/(input(2)+input(4))/vehicle.GR*vehicle.R;
+    Tmotor(4) = totalFxRight*input(4)/(input(2)+input(4))/vehicle.GR*vehicle.R;
 
 else % the grip limit is on the right side
-    Tmotor(2) = maxGripLimitedFx(2)*input(2)/(input(2)+input(4))/vehicle.GR*vehicle*Reff;
-    Tmotor(4) = maxGripLimitedFx(4)*input(4)/(input(2)+input(4))/vehicle.GR*vehicle*Reff;
+    Tmotor(2) = maxGripLimitedFx(2)*input(2)/(input(2)+input(4))/vehicle.GR*vehicle.R;
+    Tmotor(4) = maxGripLimitedFx(4)*input(4)/(input(2)+input(4))/vehicle.GR*vehicle.R;
     
     totalFxLeft = (maxGripLimitedFx(2)*input(2)/(input(2)+input(4)) + maxGripLimitedFx(4)*input(4)/(input(2)+input(4)))*torqueRatio;
     
     
-    Tmotor(1) = totalFxLeft*input(1)/(input(1)+input(3))/vehicle.GR*vehicle*Reff;
-    Tmotor(3) = totalFxLeft*input(3)/(input(1)+input(3))/vehicle.GR*vehicle*Reff;
+    Tmotor(1) = totalFxLeft*input(1)/(input(1)+input(3))/vehicle.GR*vehicle.R;
+    Tmotor(3) = totalFxLeft*input(3)/(input(1)+input(3))/vehicle.GR*vehicle.R;
 
 end
 
