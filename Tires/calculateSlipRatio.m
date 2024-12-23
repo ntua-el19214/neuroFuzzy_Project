5ad2ea863c6b6bf38dd6721cd18865012d398570
ntua-el19214 <108694882+ntua-el19214@users.noreply.github.com>
2024-12-23 03:04:30 +0200

    Made it actually functional and solvable with ode45

--- Tires/calculateSlipRatio.m
+++ Tires/calculateSlipRatio.m
@@ -1,12 +1,34 @@
-function slipRR = calculateSlipRatio(omega, wheelV, Reff)
-% This function calculates wheel slip ratio based on
-% the angular velocity (omega), linear speed (wheelV), and tire
-% effective radius (Reff).
+function [SR_FL,SR_FR,SR_RL,SR_RR] = calculateSlipRatio (omega_FL,omega_FR,omega_RL,omega_RR,yawrate,Vx,vehicle)
 
-% Calculate RR slip ratio
-if abs(omega * Reff) < 0.3
-    slipRR = 2 * (omega * Reff - wheelV) / (0.3 + wheelV^2 / 0.3);
-else
-    slipRR = (omega * Reff - wheelV) / abs(wheelV);
+Rw = vehicle.R;
+tf = vehicle.tf;
+tr = vehicle.tf;
+
+VxFR = Vx+0.5*yawrate*tf;
+VxFL = Vx-0.5*yawrate*tf;
+VxRR = Vx+0.5*yawrate*tr;
+VxRL = Vx-0.5*yawrate*tr;
+
+smol = 0.01;
+SR_FR = (omega_FR*Rw- VxFR)/(VxFR + smol);
+SR_FL = (omega_FL*Rw- VxFL)/(VxFL + smol);
+SR_RR = (omega_RR*Rw- VxRR)/(VxRR + smol);
+SR_RL = (omega_RL*Rw- VxRL)/(VxRL + smol);
+
+minrot = 1;
+if abs(omega_FR)< minrot && abs(VxFR)<0.1
+    SR_FR = 0;
+end
+
+if abs(omega_FL)< minrot && abs(VxFL)<0.1
+    SR_FL = 0;
+end
+
+if abs(omega_RR)< minrot && abs(VxRR)<0.1
+    SR_RR = 0;
+end
+
+if abs(omega_RL)< minrot && abs(VxRL)<0.1
+    SR_RL = 0;
 end
 end
--- Vehicle/nonLinearVehicleModel.m
+++ Vehicle/nonLinearVehicleModel.m
@@ -1,25 +1,16 @@
-function [x_dot, Tmotor]= nonLinearVehicleModel(t, Y, delta, ax, ay, vehicle, ssVectorSA, fxSS ,ExpandedMatrices)
+function [x_dot, aux]= nonLinearVehicleModel(t, Y, delta, vehicle, ssVectorSA, fxSS ,ExpandedMatrices)
 % NONLINEARVEHICLEMODEL describes the non-linear equations that dictate vehicle dynamics
 
 % Initialize integral term for yaw control
-% persistent ax ay
-% if isempty(ax)
-%     ax = 0;
-% end
-% if isempty(ay)
-%     ay = 0;
-% end
+persistent ax ay
+if isempty(ax)
+    ax = 0;
+end
+if isempty(ay)
+    ay = 0;
+end
 
 g = 9.81; % Gravitational acceleration
-if t > 0.26
-    peos = 7;
-    if t> 0.28
-        peos = 9;
-        if t> 0.3
-            peos = 10;
-        end
-    end
-end
 
 % Extract state variables
 psi_dot  = Y(1);  % Yaw rate
@@ -53,15 +44,7 @@ Fz_rr = vehicle.m * g * (1 - vehicle.wd) / 2 - (vehicle.CoGz * vehicle.m * ay / 
         (vehicle.CoGz * vehicle.m * ax / (2 * vehicle.wb)) + 1/2 * 1/4 * 1.22 * vehicle.cl * vx^2;
 
 % Calculate slip ratios and slip angles
-speedFL = round((vx - psi_dot * br / 2 * cos(delta) + (vy + psi_dot * lf) * sin(delta)), 5);
-speedFR = round((vx + psi_dot * br / 2 * cos(delta) + (vy + psi_dot * lf) * sin(delta)), 5);
-speedRL = round((vx - psi_dot * br / 2), 5);
-speedRR = round((vx + psi_dot * bf / 2), 5);
-
-slip_FL = calculateSlipRatio(omega_FL, speedFL, vehicle.R);
-slip_FR = calculateSlipRatio(omega_FR, speedFR, vehicle.R);
-slip_RL = calculateSlipRatio(omega_RL, speedRL, vehicle.R);
-slip_RR = calculateSlipRatio(omega_RR, speedRR, vehicle.R);
+[slip_FL,slip_FR,slip_RL,slip_RR] = calculateSlipRatio(omega_FL, omega_FR, omega_RL, omega_RR, psi_dot,vx,vehicle);
 
 slipAngle_FR = atan((vy + psi_dot*lf)/(vx + 0.5*psi_dot*bf)) - delta;
 slipAngle_FL = atan((vy + psi_dot*lf)/(vx - 0.5*psi_dot*bf)) - delta;
@@ -84,9 +67,11 @@ vx_dot = 1 / vehicle.m * ((Fx_fl + Fx_fr)*cos(delta) + Fx_rl + Fx_rr...
                            -(Fy_fl + Fy_fr)*sin(delta)  ...
                            - 1/2 * 1.224 * vehicle.cd * vx^2) + psi_dot*vy;
                             
+ax = vx_dot;
 vy_dot = 1 / vehicle.m * ((Fx_fl + Fx_fr)*sin(delta) ...
                            +(Fy_fl + Fy_fr)*cos(delta) + Fy_rl + Fy_rr)...
                            - psi_dot*vx;
+ay = vy_dot;
 
 % ψ_ddot (yaw acceleration)
 psi_ddot = (1 / vehicle.Jz) * (lf * ((Fx_fl + Fx_fr) * sin(delta) + (Fy_fl + Fy_fr) * cos(delta)) - ...
@@ -98,7 +83,7 @@ psi_dot_desired = sqrt(vx^2 + vy^2) * delta / (lf + lr);
 yaw_error = psi_dot_desired - psi_dot;
 
 gainStruct = gainScheduling(delta, ssVectorSA, ExpandedMatrices);
-input = yaw_error * gainStruct.Kp + i_error * gainStruct.Ki - gainStruct.Kr * [b; psi_dot] +fxSS;
+input = +yaw_error * gainStruct.Kp + i_error * gainStruct.Ki - gainStruct.Kr * [b; psi_dot] +fxSS;
 % input = yaw_error * gainStruct.Kp + i_error * gainStruct.Ki +fxSS;
 slipAngleMatrix = [slipAngle_FL, slipAngle_FR, slipAngle_RL, slipAngle_RR];
 fzMatrix = [Fz_fl, Fz_fr, Fz_rl, Fz_rr];
@@ -114,7 +99,6 @@ omega_FR_dot = (Tmotor(2)*vehicle.GR - Fx_fr * vehicle.R - 0.03*Fz_fr) / vehicle
 omega_RL_dot = (Tmotor(3)*vehicle.GR - Fx_rl * vehicle.R - 0.03*Fz_rl) / vehicle.Jw;
 omega_RR_dot = (Tmotor(4)*vehicle.GR - Fx_rr * vehicle.R - 0.03*Fz_rr) / vehicle.Jw;
 
-
 % Return the state derivatives
 x_dot = [psi_ddot;   % Change in yaw rate
          vx_dot;     % Change in velocity
@@ -127,4 +111,17 @@ x_dot = [psi_ddot;   % Change in yaw rate
          vx;
          vy;
          psi_dot];
+
+
+aux = struct();
+aux.slipAngles = struct('FL', slipAngle_FL, 'FR', slipAngle_FR, 'RL', slipAngle_RL, 'RR', slipAngle_RR);
+aux.slipRatios = struct('FL', slip_FL, 'FR', slip_FR, 'RL', slip_RL, 'RR', slip_RR);
+aux.Forces = struct('Fx', struct('FL', Fx_fl, 'FR', Fx_fr, 'RL', Fx_rl, 'RR', Fx_rr), ...
+                    'Fy', struct('FL', Fy_fl, 'FR', Fy_fr, 'RL', Fy_rl, 'RR', Fy_rr), ...
+                    'Fz', struct('FL', Fz_fl, 'FR', Fz_fr, 'RL', Fz_rl, 'RR', Fz_rr));
+aux.MotorTorques = struct('FL', Tmotor(1), 'FR', Tmotor(2), 'RL', Tmotor(3), 'RR', Tmotor(4));
+aux.SlipAngles   = struct('FL', slipAngle_FL, 'FR', slipAngle_FR, 'RL', slipAngle_RL, 'RR', slipAngle_RR);
+aux.SlipRatios   = struct('FL', slip_FL, 'FR', slip_FR, 'RL', slip_RL, 'RR', slip_RR);
+aux.yawError 	 = yaw_error;
+
 end
